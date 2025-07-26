f6ec3f2189e79dec44c8b5ffb1d3adad6df14803
wellenzheng <40078093+wellenzheng@users.noreply.github.com>
2025-07-26 17:52:00 +0800

    chore: thinking (#18)
    Co-authored-by: zhengweijun <weijun.zheng@aminer.cn>

---
+++ tests/integration_tests/test_thinking.py
@@ -0,0 +1,59 @@
+import logging
+import logging.config
+import time
+
+import zai
+from zai import ZaiClient
+
+def test_chat_completion_with_thinking(logging_conf):
+    logging.config.dictConfig(logging_conf)  # type: ignore
+    client = ZaiClient()  # Fill in your own API Key
+    try:
+        # Generate request_id
+        request_id = time.time()
+        print(f'request_id:{request_id}')
+        response = client.chat.completions.create(
+            request_id=request_id,
+            model='glm-4.5',
+            messages=[
+                {'role': 'user', 'content': '请介绍一下Agent的原理，并给出详细的推理过程'}
+            ],
+            top_p=0.7,
+            temperature=0.9,
+        )
+        print(response)
+
+    except zai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zai.core._errors.APIInternalError as err:
+        print(err)
+    except zai.core._errors.APIStatusError as err:
+        print(err)
+        
+def test_chat_completion_without_thinking(logging_conf):
+    logging.config.dictConfig(logging_conf)  # type: ignore
+    client = ZaiClient()  # Fill in your own API Key
+    try:
+        # Generate request_id
+        request_id = time.time()
+        print(f'request_id:{request_id}')
+        response = client.chat.completions.create(
+            request_id=request_id,
+            model='glm-4.5',
+            messages=[
+                {'role': 'user', 'content': '请介绍一下Agent的原理'}
+            ],
+            top_p=0.7,
+            temperature=0.9,
+            thinking={
+                "type": "disabled",
+            }
+        )
+        print(response)
+
+    except zai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zai.core._errors.APIInternalError as err:
+        print(err)
+    except zai.core._errors.APIStatusError as err:
+        print(err)
